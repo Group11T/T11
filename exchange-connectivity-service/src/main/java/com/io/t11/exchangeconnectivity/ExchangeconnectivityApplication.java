810a472fefcb60d8c7a70b623608c69cff727386
kbbansi <kwabenaampofo5@gmail.com>
2021-03-12 15:15:06 +0000

    added exchangeConnectivity service

---
+++ exchange-connectivity-service/src/main/java/com/io/t11/exchangeconnectivity/ExchangeconnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.io.t11.exchangeconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeconnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeconnectivityApplication.class, args);
+	}
+
+}
