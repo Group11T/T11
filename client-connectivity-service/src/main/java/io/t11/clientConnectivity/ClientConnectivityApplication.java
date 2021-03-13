01ca64f45be75072ce87bb6fa248669a45abbd5c
godfather <ankamahgodson@yahoo.com>
2021-03-13 15:34:18 +0000

    Initial configuration files added

---
+++ client-connectivity-service/src/main/java/io/t11/clientConnectivity/ClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package io.t11.clientConnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectivityApplication.class, args);
+	}
+
+}
