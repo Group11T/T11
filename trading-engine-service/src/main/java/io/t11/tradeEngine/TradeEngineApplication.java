5ffdabe58c26b7bac38a35e0aabb00250faec4d2
godfather <ankamahgodson@yahoo.com>
2021-03-13 16:06:52 +0000

    Initial config files added

---
+++ trading-engine-service/src/main/java/io/t11/tradeEngine/TradeEngineApplication.java
@@ -0,0 +1,13 @@
+package io.t11.tradeEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeEngineApplication.class, args);
+	}
+
+}
